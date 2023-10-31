ca9d981cbcd5b20e5daf5942cc47d9c9f3d1b494
kourosh <kooroush33@yahoo.com>
2023-10-31 11:13:58 +0330

    Add ManageShop Page

--- database/migrations/2023_10_31_060738_create_shops_table.php
+++ database/migrations/2023_10_31_060738_create_shops_table.php
@@ -4,8 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
@@ -13,6 +12,12 @@ return new class extends Migration
     {
         Schema::create('shops', function (Blueprint $table) {
             $table->id();
+            $table->unsignedInteger('user_id');
+            $table->string('title');
+            $table->string('first_name');
+            $table->string('last_name');
+            $table->string('PhoneNumber');
+            $table->text('address')->nullable();
             $table->timestamps();
         });
     }
--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -35,7 +35,14 @@
 
             <!-- Page Content -->
             <main>
-                {{ $slot }}
+                <div class="py-12">
+                    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                        <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg p-2">
+                            {{ $slot }}
+                        </div>
+                    </div>
+                </div>
+
             </main>
         </div>
 
