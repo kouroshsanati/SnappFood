ccdc6c622e81059822fcbee959b462c3d59bb27f
kourosh <kooroush33@yahoo.com>
2023-12-13 00:24:43 +0330

    fix: report view

--- resources/views/reports/index.blade.php
+++ resources/views/reports/index.blade.php
@@ -1,31 +1,37 @@
 <x-app-layout>
-    <div class="container">
-        <h1>Reports</h1>
+
+    <div class="container mt-4 mx-auto text-center">
+        <h1 class="mb-4">Reports</h1>
 
         <div class="mb-4">
-            <p>Total Orders: {{ count($reports) }}</p>
-            <p>Total Price: {{ $reports->sum('price_total') }}</p>
+            <p class="lead">Total Orders: {{ count($reports) }}</p>
+            <p class="lead">Total Price: {{ $reports->sum('price_total') }}</p>
         </div>
 
-        <table class="table">
-            <thead>
-            <tr>
-                <th>User Name</th>
-                <th>Total Price</th>
-                <th>Food Name</th>
-                <th>Quantity</th>
-            </tr>
-            </thead>
-            <tbody>
-            @foreach($reports as $report)
+        <div class="table-responsive mx-auto my-5 p-6 bg-gray-200 w-full">
+            <table class="table table-bordered table-striped text-center mx-auto">
+                <thead class="thead-dark">
                 <tr>
-                    <td>{{ $report->user_name }}</td>
-                    <td>{{ $report->price_total }}</td>
-                    <td>{{ $report->food_name }}</td>
-                    <td>{{ $report->count }}</td>
+                    <th scope="col">User Name</th>
+                    <th scope="col">Total Price</th>
+                    <th scope="col">Food Name</th>
+                    <th scope="col">Quantity</th>
                 </tr>
-            @endforeach
-            </tbody>
-        </table>
+                </thead>
+                <tbody>
+                @foreach($reports as $report)
+                    <tr>
+                        <td>{{ $report->user_name }}</td>
+                        <td>{{ $report->price_total }}</td>
+                        <td>{{ $report->food_name }}</td>
+                        <td>{{ $report->count }}</td>
+                    </tr>
+                @endforeach
+                </tbody>
+            </table>
+        </div>
+
+
     </div>
+
 </x-app-layout>
